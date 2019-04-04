8e201687b0a8d19970b2e984f24dddb8e06d0a90
Ewan Parker <34030942+ewpa@users.noreply.github.com>
2019-04-04 15:13:13 +0100

    Add support for the Microchip LAN8742 Ethernet PHY chip, including WoL.

---
+++ tools/sdk/include/ethernet/eth_phy/phy_lan8742.h
@@ -0,0 +1,82 @@
+// Copyright 2015-2017 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "phy.h"
+
+/** @brief For debugging, return the values of all registers.
+ */
+void phy_lan8742_read_registers(uint16_t values[32]);
+
+/** @brief For debugging, return the value of a chosen MMD register.
+ */
+uint16_t phy_lan8742_read_mmd_register(uint16_t devad, uint16_t index);
+
+/** @brief Dump all LAN8742 PHY SMI configuration registers
+ *
+ * @note These registers are dumped at 'debug' level, so output
+ * may not be visible depending on default log levels.
+ */
+void phy_lan8742_dump_registers();
+
+/** @brief Default LAN8742 phy_check_init function.
+ */
+void phy_lan8742_check_phy_init(void);
+
+/** @brief Default LAN8742 phy_get_speed_mode function.
+ */
+eth_speed_mode_t phy_lan8742_get_speed_mode(void);
+
+/** @brief Default LAN8742 phy_get_duplex_mode function.
+ */
+eth_duplex_mode_t phy_lan8742_get_duplex_mode(void);
+
+/** @brief Default LAN8742 phy_power_enable function.
+ *
+ * @note This function may need to be replaced with a custom function
+ * if the PHY has a GPIO to enable power or start a clock.
+ *
+ * Consult the ethernet example to see how this is done.
+ */
+void phy_lan8742_power_enable(bool);
+
+/** @brief Default LAN8742 phy_init function.
+ */
+void phy_lan8742_init(void);
+
+/** @brief LAN8742 Wake-On-LAN magic packet enable function.
+ */
+void phy_lan8742_wol_magic_begin(void);
+
+/** @brief LAN8742 Wake-On-LAN magic packet disable function.
+ */
+void phy_lan8742_wol_magic_end(void);
+
+/** @brief Default LAN8742 PHY configuration
+ *
+ * This configuration is not suitable for use as-is, it will need
+ * to be modified for your particular PHY hardware setup.
+ *
+ * Consult the Ethernet example to see how this is done.
+ */
+extern const eth_config_t phy_lan8742_default_ethernet_config;
+
+#ifdef __cplusplus
+}
+#endif
